ba87337a639b0f0fb070ed80067910ba7802a9ed
Luis <ramirezromero.le@gmail.com>
2025-12-05 02:38:46 -0500

    <feature/reportes-de-emprendimientos> desarrollo de reportes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+
 const authRoutes = require("./auth.routes");
 const productRoutes = require("./products.routes");
-const notificationsRoutes = require("./notifications.routes"); 
+const reportesRoutes = require("./reportes");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas de autenticación (login / registro)
 app.use("/auth", authRoutes);
-// Rutas de productos
 app.use("/products", productRoutes);
-// Rutas de notificaciones
-app.use("/notifications", notificationsRoutes);
+app.use("/reportes", reportesRoutes);
 
-// Ruta de prueba
 app.get("/", (req, res) => {
-  res.json({ message: "Backend del proyecto emprendeUIDE móvil funcionando " });
+  res.json({ message: "Backend del proyecto emprendeUIDE móvil funcionando" });
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
