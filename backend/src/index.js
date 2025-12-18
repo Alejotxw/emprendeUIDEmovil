902d14e257345261c5ca6aa86c448109fa624f6d
Alejotxw <sebas.a2035@gmail.com>
2025-12-18 18:08:24 -0500

    <actualizacion>

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./auth.routes");
 const productRoutes = require("./products.routes");
+<<<<<<< Updated upstream
+=======
+const notificationsRoutes = require("./notifications.routes"); 
+>>>>>>> Stashed changes
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,6 +18,11 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 // Rutas de productos
 app.use("/products", productRoutes);
+<<<<<<< Updated upstream
+=======
+// Rutas de notificaciones
+app.use("/notifications", notificationsRoutes);
+>>>>>>> Stashed changes
 
 // Ruta de prueba
 app.get("/", (req, res) => {
